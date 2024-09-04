01be0fe2c711226b0d42be5f6ea16b86938010f3
Adam D. <adam@adamduda.com>
2024-09-03 20:08:03 -0500

    lifestone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            TEST54321 Learn about Next.js in an interactive course with&nbsp;quizzes!
+            HELLO WORLD Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
         </a>
 
